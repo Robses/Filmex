ecbd5138ba1496d8594859940804bcd3a0241af6
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-04-22 00:16:27 +0200

    closes #10, closes #16, closes #18

--- Movie.js
+++
@@ -1,5 +0,0 @@
-class Movie {
-    constructor(title) {
-        this.title = title;
-    }
-}
---
+++ models/GeneralData.js
@@ -0,0 +1,33 @@
+class GeneralData {
+    constructor(title, year, rated, released, runtime, 
+                genre, director, writer, actors, plot, 
+                language, country, awards, poster, ratings, 
+                metascore, imdbRating, imdbID, type, DBD, 
+                boxOffice, production, website, response) 
+    {
+        this.title = title;
+        this.year = year;
+        this.rated = rated;
+        this.released = released;
+        this.runtime = runtime;
+        this.genre = genre;
+        this.director = director;
+        this.writer = writer;
+        this.actors = actors;
+        this.plot = plot;
+        this.language = language;
+        this.country = country;
+        this.awards = awards;
+        this.poster = poster;
+        this.ratings = ratings;
+        this.metascore = metascore;
+        this.imdbRating = imdbRating;
+        this.imdbID = imdbID;
+        this.type = type;
+        this.DBD = DBD;
+        this.boxOffice = boxOffice;
+        this.production = production;
+        this.website = website;
+        this.response = response;
+    }
+}
